791b283ea91b8fb52aa24ea535cb2ab1e44e4ea7
Alex <alex_mcgarry@hotmail.co.uk>
2024-08-30 16:47:48 +0100

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,21 @@ export const metadata: Metadata = {
   title: 'Brighton Rock Housing Co-operative',
   description:
     'Brighton Rock is a small housing co-operative in West Hove that provides affordable housing to its members.',
+  openGraph: {
+    title: 'Brighton Rock Housing Co-operative',
+    description:
+      'Brighton Rock is a small housing co-operative in West Hove that provides affordable housing to its members. Please visit our website for more information and current vacancies.',
+    url: 'https://brighton-rock.org/',
+    images: [
+      {
+        url: 'https://d33wubrfki0l68.cloudfront.net/45dc7e2de3f6be14d03156f17331b5b091c918ab/cfeab/images/co-op50.webp',
+        width: 800,
+        height: 600,
+        alt: 'Brighton Rock Housing Co-operative',
+      },
+    ],
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
@@ -28,6 +43,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning className={roboto.className}>
+      <head>
+        {/* Meta tags for Open Graph */}
+        <meta
+          property="og:title"
+          content="Brighton Rock Housing Co-operative"
+        />
+        <meta
+          property="og:description"
+          content="Brighton Rock is a small housing co-operative in West Hove that provides affordable housing to its members. Please visit our website for more information and current vacancies."
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/drbz4rq7y/image/upload/v1724979773/coop-images/co-op50_v0fqdb.webp"
+        />
+        <meta property="og:url" content="https://brighton-rock.org/" />
+        <meta property="og:type" content="website" />
+
+        {/* Additional metadata tags */}
+        <link rel="icon" href="/images/favicon.png" />
+        <title>Brighton Rock Housing Co-operative</title>
+      </head>
       <body>
         {/* <ThemeEditor /> */}
         <ThemeProvider
@@ -38,6 +74,7 @@ export default function RootLayout({
         >
           <Header />
           <main>{children}</main>
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
