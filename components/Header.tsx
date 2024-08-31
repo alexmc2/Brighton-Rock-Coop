21b467481abae8948e0e9d1594511e361883102e
Alex <alex_mcgarry@hotmail.co.uk>
2024-08-31 10:41:19 +0100

    adjust header on mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -32,7 +32,7 @@ const Header: React.FC = () => {
       )}
     >
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-20">
+        <div className="flex items-center justify-between h-14 sm:h-16 md:h-20">
           <div className="flex items-center">
             <ThemeToggle />
             <Link
